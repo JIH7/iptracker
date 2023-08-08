6368ad328456849b93c61cf57605d5a7934071f8
Jeremy Helsel <JIHlucky7@gmail.com>
2023-08-08 00:29:33 -0500

    Building out the layout

---
+++ main.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([41.905062, -87.692431], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
